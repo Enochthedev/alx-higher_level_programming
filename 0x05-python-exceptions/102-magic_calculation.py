c34fa293f123b471a71c823b98c45f286530bdc5
Toluwani omosebi <enochjesse884@outlook.com>
2023-01-23 22:02:39 +0100

    102 changed

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -2,15 +2,18 @@
 # 102-magic_calculation.py
 
 
+
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
         try:
             if i > a:
-                raise Exception("Too far")
+                raise Exception('Too far')
             else:
-                result = result + (a ** b) / i
+                result += a ** b / i
         except:
-            result = result + b
-            continue
-    return result
+            result = b + a
+            break
+    return (result)
+
+
