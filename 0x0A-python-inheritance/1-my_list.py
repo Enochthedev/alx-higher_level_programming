68bf08a71bd97a87b53ca5c4eeb64a902cdb156d
Toluwani omosebi <enochjesse884@outlook.com>
2023-02-07 08:25:12 +0100

    tests removed

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -10,7 +10,19 @@ Module with class MyList
 
 class MyList(list):
     """Class with method print_sorted"""
+    pass
     def print_sorted(self):
         """Methot that sorted a list"""
 
-        print(sorted(self))
+        print(sorted(list(self)))
+
+
+my_list = MyList()
+my_list.append(1)
+my_list.append(4)
+my_list.append(2)
+my_list.append(3)
+my_list.append(5)
+print(my_list)
+my_list.print_sorted()
+print(my_list)
