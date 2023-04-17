4ebcb2cda0681c1135aa33f39c72fe7669ecd74f
Enochthedev <enochjesse884@gmail.com>
2023-04-18 00:54:03 +0100

    0-3 fixed

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,36 @@
 #!/usr/bin/python3
+"""
+Lists all states from the database hbtn_0e_0_usa sorted in ascending order by
+states.id
+"""
 import MySQLdb
 import sys
 
-if __name__ == '__main__':
-    # Connect to MySQL server running on localhost at port 3306
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-    # Create cursor
-    cursor = db.cursor()
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    db_name = sys.argv[3]
 
-    # Execute SQL query to select all states
-    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    try:
+        conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_username,
+            passwd=mysql_password,
+            db=db_name,
+            charset="utf8"
+        )
+    except MySQLdb.Error as e:
+        print("Error connecting to database: {}".format(e))
+        sys.exit(1)
 
-    # Fetch all rows and print them
-    rows = cursor.fetchall()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+
     for row in rows:
         print(row)
 
-    # Close cursor and database connection
-    cursor.close()
-    db.close()
+    cur.close()
+    conn.close()
